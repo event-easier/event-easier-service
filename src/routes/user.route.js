c343da483efa68eb352a850278251b539803addb
MSI\trung <bin.trung0@gmail.com>
2023-05-14 06:58:16 +0700

    login and register

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { updateById, deleteById } from "../controllers/users.controller.js";
+import { updateById, deleteById, login, register } from "../controllers/users.controller.js";
 
 const userRouter = Router();
 
 userRouter.put("/:id", updateById);
 userRouter.delete("/:id", deleteById);
+userRouter.post("/", login);
+userRouter.post("/register", register);
 
 export default userRouter;
