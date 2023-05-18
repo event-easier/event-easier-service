1c0c811cc5110dcb0d7e1f050de9805828ac72ba
MSI\trung <bin.trung0@gmail.com>
2023-05-18 22:27:42 +0700

    done veryfi

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { updateById, deleteById, login, register } from "../controllers/users.controller.js";
+import { updateById, deleteById, login, register, verifyUser } from "../controllers/users.controller.js";
+import { sendEmail } from "../utils/email.utils.js";
 
 const userRouter = Router();
 
 userRouter.put("/:id", updateById);
 userRouter.delete("/:id", deleteById);
-userRouter.post("/", login);
+userRouter.post("/login", login);
+userRouter.post("/login/verify", verifyUser);
 userRouter.post("/register", register);
 
 export default userRouter;
