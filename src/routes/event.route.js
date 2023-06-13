a56c0934cb116721eec44a023109b5526ce7c5c0
tranminhwang <j2team.tranminhquang@gmail.com>
2023-06-13 19:56:50 +0700

    add middleware

--- src/routes/event.route.js
+++ src/routes/event.route.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
-import { create,findOne, findAll, updateById } from "../controllers/events.controller.js";
+import {
+  create,
+  findOne,
+  findAll,
+  updateById,
+} from "../controllers/events.controller.js";
 import { checkLogin } from "../middlewares/auth.middleware.js";
 
 const eventRouter = Router();
 
-eventRouter.get("/:id", findOne);
-eventRouter.post("/user", findAll);
-eventRouter.post("/create",checkLogin ,create);
-eventRouter.post("/update/:id",checkLogin ,updateById);
+eventRouter.get("/:id", checkLogin, findOne);
+eventRouter.post("/user", checkLogin, findAll);
+eventRouter.post("/create", checkLogin, create);
+eventRouter.post("/update/:id", checkLogin, updateById);
 
 export default eventRouter;
