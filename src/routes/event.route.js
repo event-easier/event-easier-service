f9ed768175d91bfbc78dc5c19b10859709cfb907
tranminhwang <j2team.tranminhquang@gmail.com>
2023-07-04 20:16:16 +0700

    update method

--- src/routes/event.route.js
+++ src/routes/event.route.js
@@ -14,7 +14,7 @@ import { checkLogin } from "../middlewares/auth.middleware.js";
 const eventRouter = Router();
 
 eventRouter.get("/:id", findOne);
-eventRouter.post("/user", checkLogin, findAll);
+eventRouter.get("/user", checkLogin, findAll);
 eventRouter.post("/create", checkLogin, create);
 eventRouter.post("/update/:id", checkLogin, updateById);
 eventRouter.post("/inviteGuests/:id", checkLogin, inviteGuests);
