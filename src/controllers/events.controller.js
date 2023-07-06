fc22813f864a36233246f8f05aaac4e017a08cfb
khoa <khoait1986@gmail.com>
2023-07-06 14:26:58 +0700

    fixed event registration

--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -100,7 +100,7 @@ export const findOne = (req, res) => {
 };
 
 export const findAll = (req, res) => {
-  const user_id = req.userid;
+  const user_id = req.userId;
   // create an condition
   const condition = {
     $or: [
